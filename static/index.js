690698f6b7144488806ffd2856f42faccae37147
yjhouma <yjhouma@gmail.com>
2024-07-25 12:01:03 +0700

    using-tailwind

--- static/index.js
+++ static/index.js
@@ -16,22 +16,34 @@ async function fetchYamlData(filename) {
     }
 }
 
+
+// {/* <h3>${exp.title}</h3>
+//             <h4>${exp.company} - ${exp.location}</h4>
+//             <p><em>${exp.period}</em></p>
+//             <ul>
+//                 ${exp.responsibilities.map(resp => `<li>${resp}</li>`).join('')}
+//             </ul> */}
+
 // Function to create and append experience items
 async function populateExperiences() {
-    const experienceSection = document.getElementById('experience');
-    const experiences = await fetchYamlData('data/experiences.yaml');
+    const experienceSection = document.getElementById('experience-container');
+    const experiences = await fetchYamlData('static/data/experiences.yaml');
     
     if (!experiences) return;
 
     experiences.forEach(exp => {
         const expDiv = document.createElement('div');
-        expDiv.className = 'experience';
+        expDiv.className = 'bg-gray-50 shadow-md rounded-lg p-6';
         expDiv.innerHTML = `
-            <h3>${exp.title}</h3>
-            <h4>${exp.company} - ${exp.location}</h4>
-            <p><em>${exp.period}</em></p>
-            <ul>
-                ${exp.responsibilities.map(resp => `<li>${resp}</li>`).join('')}
+            <h3 class="text-xl font-semibold text-gray-800 mb-1">${exp.title}</h3>
+            <p class="text-gray-600 mb-1">${exp.company} - ${exp.location}</p>
+            <p class="text-sm text-gray-500 italic mb-4">${exp.period}</p>
+            <ul class="list-disc list-outside space-y-1 text-gray-700">
+                ${exp.responsibilities.map(resp => `
+                    <li>
+                        ${resp}
+                    </li>
+                `).join('')}
             </ul>
         `;
         experienceSection.appendChild(expDiv);
@@ -41,7 +53,7 @@ async function populateExperiences() {
 // Function to create and append project items
 async function populateProjects() {
     const projectsSection = document.getElementById('projects');
-    const projects = await fetchYamlData('data/projects.yaml');
+    const projects = await fetchYamlData('static/data/projects.yaml');
     
     if (!projects) return;
 
@@ -61,13 +73,13 @@ async function populateProjects() {
 // Function to populate skills
 async function populateSkills() {
     const skillsDiv = document.querySelector('.skills');
-    const skills = await fetchYamlData('data/skills.yaml');
+    const skills = await fetchYamlData('static/data/skills.yaml');
     
     if (!skills) return;
 
     skills.forEach(skill => {
         const skillSpan = document.createElement('span');
-        skillSpan.className = 'skill';
+        skillSpan.className = 'bg-blue-500 text-white px-2 py-1 rounded';
         skillSpan.textContent = skill;
         skillsDiv.appendChild(skillSpan);
     });
